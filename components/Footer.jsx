67a428b41b89b03d330d569fc5709055e12b4875
Thaotv <thaotv@vinorsoft.com>
2022-08-02 11:23:55 +0700

    new code 0802

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,26 +2,26 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="container-fluid footer bg-light py-5 mb-5">
+    <div className="container-fluid footer bg-dark py-5 mb-5">
       <div className="container">
         <div className="row">
-          <div className="col text-dark">
+          <div className="col-12 col-md-6 col-lg-3 text-light">
             <h5 className="text-center">Thông tin</h5>
           </div>
-          <div className="col text-dark">
+          <div className="col-12 col-md-6 col-lg-3 text-light">
             <h5 className="text-center">Hỗ trợ</h5>
 
           </div>
-          <div className="col text-dark">
+          <div className="col-12 col-md-6 col-lg-3 text-light">
             <h5 className="text-center">Kết nối</h5>
 
           </div>
-          <div className="col text-dark">
+          <div className="col-12 col-md-6 col-lg-3 text-light">
             <h5 className="text-center">Liên hệ</h5>
 
           </div>
         </div>
-        <div className="policy text-dark text-center py-2">
+        <div className="policy text-light text-center py-2">
           <p>Copyright 2022</p>
         </div>
       </div>
