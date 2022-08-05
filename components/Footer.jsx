1a133daf1987269125482697c69b5182df1f0eed
thaotran85 <thaotv@vinorsoft.com>
2022-08-05 14:32:04 +0700

    layout profile

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="container-fluid footer bg-dark py-5 mb-5">
+    <div className="container-fluid footer bg-dark py-5 mb-5 position-absolute bottom-0">
       <div className="container">
         <div className="row">
           <div className="col-12 col-md-6 col-lg-3 text-light">
@@ -10,15 +10,12 @@ const Footer = () => {
           </div>
           <div className="col-12 col-md-6 col-lg-3 text-light">
             <h5 className="text-center">Hỗ trợ</h5>
-
           </div>
           <div className="col-12 col-md-6 col-lg-3 text-light">
             <h5 className="text-center">Kết nối</h5>
-
           </div>
           <div className="col-12 col-md-6 col-lg-3 text-light">
             <h5 className="text-center">Liên hệ</h5>
-
           </div>
         </div>
         <div className="policy text-light text-center py-2">
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,42 +1,50 @@
 import Link from "next/link";
 import React, { useContext, useEffect, useState } from "react";
 import { BsSearch, BsCartCheck } from "react-icons/bs";
-import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from "react-bootstrap/Dropdown";
 import { isMobile } from "react-device-detect";
-import Logo from '../public/image/logo.jpg'
+import Logo from "../public/image/logo.jpg";
 import { useRouter } from "next/router";
 import { DataContext } from "../store/globalState";
 import LoginModal from "./LoginModal";
 const Navbar = () => {
-  const { state, dispatch } = useContext(DataContext)
-  const [username, setusername] = useState()
+  const { state, dispatch } = useContext(DataContext);
+  const [username, setusername] = useState();
   useEffect(() => {
-    let name = JSON.parse(localStorage.getItem('userInfo')) || ''
+    let name = JSON.parse(localStorage.getItem("userInfo")) || "";
     if (name) {
-      setusername(name.fullName)
+      setusername(name.fullName);
     }
-  }, [])
+  }, []);
 
-  const router = useRouter()
+  const router = useRouter();
   const handleLogOut = () => {
-    localStorage.removeItem('userInfo')
-    localStorage.removeItem('token')
-    dispatch({ type: 'NOTIFY', payload: { success: 'success' } })
-    router.push('/')
+    localStorage.removeItem("userInfo");
+    localStorage.removeItem("token");
+    dispatch({ type: "NOTIFY", payload: { success: "success" } });
+    router.push("/");
     window.location.reload();
-}
+  };
 
   const [_isMobile, setMobile] = useState(false);
   useEffect(() => {
-      setMobile(isMobile);
+    setMobile(isMobile);
   }, [_isMobile]);
   return (
-    <div className="container-fluid mb-2 bg-white position-fixed top-0 start-0" style={{ zIndex: 99 }} id="nav">
+    <div
+      className="container-fluid mb-2 bg-white position-fixed top-0 start-0"
+      style={{ zIndex: 99 }}
+      id="nav"
+    >
       <div className="container ">
         <div className="row py-2">
           <div className="col-3 col-md-2 col-lg-1 d-flex">
             <a className="navbar-brand text-light align-self-center " href="#">
-              <img src='/image/logo.jpg' alt="" style={{ width: '100%', objectFit: 'cover', height: '40%', display: 'block' }} />
+              <img
+                src="/image/logo.jpg"
+                alt=""
+                style={{ width: "100%", objectFit: "cover", height: "40%", display: "block" }}
+              />
             </a>
           </div>
           <div className="col-3 col-md-7 col-lg-8 offset-lg-1 px-0">
@@ -62,35 +70,43 @@ const Navbar = () => {
                 <BsCartCheck style={{ fontSize: 24, color: "#000" }} />
               </a>
             </Link> */}
-            {username &&  !_isMobile ?
-                <Dropdown>
+            {username && !_isMobile ? (
+              <Dropdown>
                 <Dropdown.Toggle variant="success" id="dropdown-menu-align-responsive-1">
-                    <span className="text-light bg-secondary hideOnMB" style={{
-                      width: 30, height: '30px',
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      border: '1px solid #fff', borderRadius: 50, textAlign: 'center', padding: 5
-                    }}>{username && username.charAt(0)}</span>
+                  <span
+                    className="text-light bg-secondary hideOnMB"
+                    style={{
+                      width: 30,
+                      height: "30px",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      border: "1px solid #fff",
+                      borderRadius: 50,
+                      textAlign: "center",
+                      padding: 5,
+                    }}
+                  >
+                    {username && username.charAt(0)}
+                  </span>
                 </Dropdown.Toggle>
-          
+
                 <Dropdown.Menu>
                   <Dropdown.Item href="/profile">Thông tin tài khoản</Dropdown.Item>
                   <Dropdown.Item href="#">Đơn hàng</Dropdown.Item>
                   <Dropdown.Item href="#">Booking</Dropdown.Item>
-                  <Dropdown.Item  onClick={handleLogOut} >Đăng xuất</Dropdown.Item>
+                  <Dropdown.Item onClick={handleLogOut}>Đăng xuất</Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
-                : ''
-            }
+            ) : (
+              ""
+            )}
 
-            {!username &&
-                <LoginModal></LoginModal>
-            }
+            {!username && <LoginModal></LoginModal>}
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 };
 
