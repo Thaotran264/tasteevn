1fa221b0cdd7b59ca6a7f6e492da9d167973d4fa
thaotran85 <thaotv@vinorsoft.com>
2022-08-24 12:27:32 +0700

    fix err

--- components/Toast.jsx
+++ components/Toast.jsx
@@ -5,22 +5,20 @@ const Toast = ({ msg, handleShow, bgColor }) => {
   const { state, dispatch } = useContext(DataContext);
   useEffect(() => {
     let idTime = setTimeout(() => {
-      dispatch({ type: "NOTIFY", payload: {} })
-    }, 2500)
+      dispatch({ type: "NOTIFY", payload: {} });
+    }, 2500);
 
     return () => {
-      clearTimeout(idTime)
-    }
-  }, [])
-  
+      clearTimeout(idTime);
+    };
+  }, []);
+
   return (
     <div
-      className={`toast show position-fixed text-light ${bgColor}`}
+      className={`toast show position-fixed text-light`}
       style={{ top: "55px", right: "5px", zIndex: 9, minWidth: "280px" }}
     >
-      <div
-        className={`toast-header ${bgColor} text-light d-flex justify-content-between`}
-      >
+      <div className={`toast-header ${bgColor} text-light d-flex justify-content-between`}>
         <strong className="mr-auto text-light">{msg.title}</strong>
 
         <button
