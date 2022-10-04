8cca28a93020793a6b2e7cffb1bb61d84022514a
dao nguyen <sydaonguyen1999@gmail.com>
2022-10-04 11:30:26 +0700

    add footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,25 +1,54 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { BsFacebook, BsYoutube } from "react-icons/bs";
+import { SiZalo } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <footer className="bg-dark bg-opacity-75 ">
+    <footer className="bg-light pt-1">
       <div className="container py-4">
         <div className="row">
-          <div className="col-6 col-md-6 col-lg-3 text-light">
-            <h5 className="text-center">Thông tin</h5>
+          <div className="col-6 col-md-6 col-lg-3 text-dark">
+            <h5 className="fw-bold">TASTEE</h5>
+            <ul class="text-footer p-1 m-1">
+              <li><span class="fw-bolder">Email: </span><a href="#">cskh@tastee.com</a></li>
+              <li><span class="fw-bolder">Điện thoại:</span><a href="#">028 xxxx xxxx</a> </li>
+              <li><span class="fw-bolder">ĐKKD:</span><a href="#">37 Hoàn Văn Thụ, P.x, Q.Phú Nhuận, HCM </a></li>
+              <li><span class="fw-bolder">Văn phòng:</span> <a href="#">Tầng 14 Tòa nhà Halo Tower, 37 Hoàn Văn Thụ, P.x, Q.Phú Nhuận, HCM </a> </li>
+            </ul>
           </div>
-          <div className="col-6 col-md-6 col-lg-3 text-light">
-            <h5 className="text-center">Hỗ trợ</h5>
+          <div className="col-6 col-md-6 col-lg-3 text-dark">
+            <h5 className="fw-bold">VỀ TASTEE</h5>
+            <ul class="text-footer p-1 m-1">
+              <li><a href="#">Giới thiệu về Tastee</a></li>
+              <li><a href="#">Tuyển dụng nhân sự</a> </li>
+              <li><a href="#">Chính sách bảo mật</a></li>
+              <li><a href="/dieu-khoan-tastee-pos">Điều khoản sử dụng</a></li>
+            </ul>
           </div>
-          <div className="col-6 col-md-6 col-lg-3 text-light">
-            <h5 className="text-center">Kết nối</h5>
+          <div className="col-6 col-md-6 col-lg-3 text-dark">
+            <h5 className="fw-bold">HỖ TRỢ KHÁCH HÀNG</h5>
+            <ul class="text-footer p-1 m-1">
+              <li><a href="#">Tra cứu hoá đơn</a></li>
+              <li><a href="#">Mua và giao nhận Online</a> </li>
+              <li><a href="#">Qui định và hình thức thanh toán</a></li>
+              <li><a href="#">Bảo hành và Bảo trì</a></li>
+              <li><a href="#">Đổi trả và Hoàn tiền</a></li>
+            </ul>
           </div>
-          <div className="col-6 col-md-6 col-lg-3 text-light">
-            <h5 className="text-center">Liên hệ</h5>
+          <div className="col-6 col-md-6 col-lg-3 text-dark text-center ">
+            <h5 className="fw-bold">KẾT NỐI VỚI CHÚNG TÔI</h5>
+            <div className="justify-content-center d-flex gap-4 fs-1">
+              <BsFacebook style={{ color: '#3b5998' }} />
+              <BsYoutube style={{ color: '#c4302b' }} />
+              <SiZalo style={{ color: '#3b5998' }} />
+            </div>
+
           </div>
         </div>
       </div>
-      <p className="py-2 text-light text-center ">Copyright 2022</p>
+      <p className="border-top py-2 text-center fw-light " style={{ fontSize: '12px' , color: '#252525' }}>© Copyright 2022 by Tastee team. All Rights Reserved</p>
     </footer>
   );
 };
--- db.js
+++ db.js
@@ -1820,3 +1820,42 @@ export const listId = [
   'tasteepos',
   'tasteePOS'
 ]
+
+
+
+export const DieuKhoanDichVu = 
+`<p><strong>Quy định sử dụng phần mềm</strong></p>
+
+<p>Kh&aacute;ch h&agrave;ng cần phải đọc v&agrave; đồng &yacute; to&agrave;n bộ điều khoản sử dụng của Tastee POS.</p>
+
+<p>Ch&uacute;ng t&ocirc;i c&oacute; thể chỉnh sửa v&agrave; bổ sung quy định về c&aacute;c điều khoản bất kỳ l&uacute;c n&agrave;o. Ch&uacute;ng t&ocirc;i mong rằng bạn sẽ thường xuy&ecirc;n xem lại điều khoản sử dụng để đảm bảo về quyền lợi của bạn trong qu&aacute; tr&igrave;nh sử dụng ứng dụng cũng như c&aacute;c quy định sử dụng v&agrave; c&aacute;c quy định về th&ocirc;ng tin đăng tải.</p>
+
+<p>Trong qu&aacute; tr&igrave;nh sử dụng, nếu kh&aacute;ch h&agrave;ng thực hiện x&oacute;a c&aacute;c th&ocirc;ng tin, h&igrave;nh ảnh li&ecirc;n quan đế t&agrave;i khoản của kh&aacute;ch h&agrave;ng th&igrave; sẽ kh&ocirc;ng thể kh&ocirc;i phục được dữ liệu.</p>
+
+<p>Trong trường hợp kh&aacute;ch h&agrave;ng x&oacute;a t&agrave;i khoản hoặc x&oacute;a cửa h&agrave;ng th&igrave; mọi th&ocirc;ng tin của kh&aacute;ch h&agrave;ng sẽ kh&ocirc;ng được bảo lưu.</p>
+
+<p><strong>Quyền sử dụng</strong></p>
+
+<p>Kh&aacute;ch h&agrave;ng c&oacute; thể sử dụng to&agrave;n bộ c&aacute;c chức năng của phần mềm hoặc t&ugrave;y theo thỏa thuận đ&atilde; k&yacute; kết của hợp đồng dịch vụ.</p>
+
+<p>Kh&aacute;ch h&agrave;ng c&oacute; quyền cập nhật v&agrave; điều chỉnh tất cả th&ocirc;ng tin sau khi đ&atilde; đăng nhập v&agrave;o ứng dụng.</p>
+
+<p>Kh&aacute;ch h&agrave;ng c&oacute; thể nhờ hỗ trợ tư vấn hoặc xem c&aacute;c hướng dẫn sử dụng phần mềm để hiểu th&ecirc;m về c&aacute;ch sử dụng ứng dụng.</p>
+
+<p>Kh&aacute;ch h&agrave;ng c&oacute; quyền x&oacute;a t&agrave;i khoản hoặc x&oacute;a cửa h&agrave;ng, trong trường hợp kh&aacute;ch h&agrave;ng x&oacute;a t&agrave;i khoản hoặc cửa h&agrave;ng th&igrave; mọi th&ocirc;ng tin của kh&aacute;ch h&agrave;ng sẽ kh&ocirc;ng được bảo lưu.</p>
+
+<p><strong>Tr&aacute;ch nhiệm</strong></p>
+
+<p>Kh&aacute;ch h&agrave;ng kh&ocirc;ng được sử dụng phần mềm với c&aacute;c mục đ&iacute;ch sau:</p>
+
+<ul>
+	<li>Kh&ocirc;ng được đăng tải c&aacute;c nội dung li&ecirc;n quan đến bạo lực.</li>
+	<li>Kh&ocirc;ng được đăng tải c&aacute;c nội dung li&ecirc;n quan đến khi&ecirc;u d&acirc;m.</li>
+	<li>Kh&ocirc;ng được đăng tải c&aacute;c nội dung với mục đ&iacute;ch lừa đảo, quấy rối hoặc l&agrave;m ảnh hưởng đến uy t&iacute;n, danh dự của người kh&aacute;c.</li>
+	<li>Kh&ocirc;ng l&agrave;m tổn hại, l&agrave;m thương tổn v&agrave; l&agrave;m phiền người kh&aacute;c về con người v&agrave; t&agrave;i sản.</li>
+	<li>Kh&ocirc;ng cố t&igrave;nh ph&aacute; hỏng dịch vụ bằng bất k&igrave; phương tiện m&aacute;y m&oacute;c hoặc phần mềm n&agrave;o.</li>
+	<li>Kh&ocirc;ng vi phạm c&aacute;c quy định của ph&aacute;p luật.</li>
+</ul>
+
+<p>Trong mọi trường hợp, Tastee POS sẽ kh&ocirc;ng phải chịu tr&aacute;ch nhiệm về những thiệt hại ph&aacute;t sinh từ việc kh&aacute;ch h&agrave;ng sử dụng dịch vụ của b&ecirc;n đối t&aacute;c thứ ba.</p>
+`
