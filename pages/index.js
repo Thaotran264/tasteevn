aadfcd694ad60ef6a11af15bd653a7d919237c4a
dao nguyen <sydaonguyen1999@gmail.com>
2022-10-09 00:38:19 +0700

    update SEO home page

--- pages/index.js
+++ pages/index.js
@@ -18,12 +18,12 @@ export default function Home({ data }) {
         <title>Tastee POS - Cộng đồng người bán</title>
         <meta property="og:type" content="article" />
         <meta property="og:title" content="Tastee POS - Cộng đồng người bán" />
-        <meta property="og:image" content="https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/306924619_7983509318387061_5915609229902822402_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=100&ccb=1-7&_nc_sid=8631f5&_nc_ohc=RKbcYTw-jUUAX9orLmK&_nc_ht=scontent.fsgn5-5.fna&oh=00_AT_yVQvee1jZsM44BMTsVRixvm3eM9MYaQ0v1G4Ixqn8cg&oe=6346619B" />
+        <meta property="og:image" content="https://tastee.vn/_next/image?url=%2Fimage%2FTastee-POS.jpeg&w=3840&q=75" />
         <meta property="og:description" content="Nhóm được xây dựng để hỗ trợ người bán hàng sử dụng ứng dụng POS của Tastee." />
         <meta property="og:url" content="https://tastee.vn" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="628" />
-        <meta property="og:image:alt " content="Tastee POS - Cộng đồng người bán" />
+        <meta property="og:image:alt" content="Tastee POS - Cộng đồng người bán" />
         <meta property="fb:app_id" content="401975274852327" />
         <meta property="article:author" content="Tastee" />
         <meta property="article:published_time" content="2022-10-08T23:26:00.000+07:00" />
@@ -36,7 +36,7 @@ export default function Home({ data }) {
         <meta name="twitter:description" content="Nhóm được xây dựng để hỗ trợ người bán hàng sử dụng ứng dụng POS của Tastee." />
         <meta name="twitter:title" content="Tastee POS - Cộng đồng người bán" />
         <meta name="twitter:site" content="@tastee" />
-        <meta name="twitter:image" content="https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/306924619_7983509318387061_5915609229902822402_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=100&ccb=1-7&_nc_sid=8631f5&_nc_ohc=RKbcYTw-jUUAX9orLmK&_nc_ht=scontent.fsgn5-5.fna&oh=00_AT_yVQvee1jZsM44BMTsVRixvm3eM9MYaQ0v1G4Ixqn8cg&oe=6346619B" />
+        <meta name="twitter:image" content="https://tastee.vn/_next/image?url=%2Fimage%2FTastee-POS.jpeg&w=3840&q=75" />
         <meta name="twitter:creator" content="@tastee" />
       </Head>
       <>
