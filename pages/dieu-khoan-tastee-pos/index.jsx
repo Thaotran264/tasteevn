83b4fb679d33b873e9c47bcf52b37febdf969e89
dao nguyen <sydaonguyen1999@gmail.com>
2022-10-08 19:16:51 +0700

    update

--- pages/dieu-khoan-tastee-pos/index.jsx
+++ pages/dieu-khoan-tastee-pos/index.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import Layout from "../../components/Layout";
 import parse from "html-react-parser";
 import { DieuKhoanDichVu } from '../../db';
+import Image from "next/image";
 
 const DieuKhoanDv = () => {
     return (
-    <div className="container">
-        {parse(DieuKhoanDichVu)}
-    </div>
+        <div className="container">
+            <a href="https://www.facebook.com/groups/tasteepos/" target='_blank'>
+                <Image className="rounded" src={'/image/Tastee-POS.jpg'} alt="First slide" width={2000} height={1000} />
+            </a>
+            {parse(DieuKhoanDichVu)}
+        </div>
     )
 }
 DieuKhoanDv.getLayout = function getLayout(Page) {
     return <Layout>{Page}</Layout>;
-  };
+};
 export default DieuKhoanDv
